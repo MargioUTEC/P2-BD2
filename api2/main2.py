f84e43eb0e438e1855f7aa7d373bd1f07924ecec
Yofer <yofer.corne@utec.edu.pe>
2025-11-26 15:56:07 -0500

    cambio

--- api2/main2.py
+++ api2/main2.py
@@ -6,3 +6,8 @@ from api2.routers.text import router as text_router
 app = FastAPI(title="Text Search API", version="1.0")
 
 app.include_router(text_router, prefix="/text")
+
+
+@app.get("/")
+def root():
+    return {"message": "Texto funcionando correctamente"}
